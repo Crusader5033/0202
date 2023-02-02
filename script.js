73ba59bdf998bf9c2530d7ee35ca162534bc4612
Dominik Procházka <prochazka6@SPSEJECNANET.CZ>
2023-02-02 07:38:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'color') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY-canvasOffsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+document.querySelector('#download').addEventListener('click', ()=> {
+    ctx.save();
+   
+  
+    
+  })
+  document.querySelector('#restore').addEventListener('click', ()=> {
+    
+   isCalledLater()
+  
+    
+  })
+
+  function isCalledLater(){
+    var canvas = document.getElementById('cv');
+    var ctx = canvas.getContext('2d');          //Get canvas and context
+    ctx.restore(); //So this should restore the canvas to what I save earlier, right?
+    ctx.save();  // this will save once again           
+    }
+
+
+canvas.addEventListener('mousemove', draw);
